efd3235d8710db4396357f7be5f54fd44075668d
Gniweist <tsiewings@gmail.com>
2016-05-20 14:59:52 +0200

    update

--- src/Application/view/TestStartMenyn.java
+++ src/Application/view/TestStartMenyn.java
@@ -1,10 +1,15 @@
 package Application.view;
 
 import javafx.application.Application; 
-import javafx.beans.value.ObservableValue; 
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
 import javafx.scene.Group; 
 import javafx.scene.Scene; 
-import javafx.scene.control.Slider; 
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color; 
 import javafx.scene.shape.Line; 
 import javafx.scene.shape.StrokeLineCap; 
@@ -12,9 +17,12 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage; 
 
 public class TestStartMenyn extends Application {
+	
+	private final TableView table = new TableView();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
 
 	}
 
@@ -23,7 +31,25 @@ public class TestStartMenyn extends Application {
 		primaryStage.setTitle("Welcome to your Pong adventure");
 		
 		Group root = new Group();
-		Scene myScene = new Scene(root, 600, 800);
+		Scene myScene = new Scene(root, 800, 600, Color.BLACK);
+		
+		final VBox vbox = new VBox(5); //space between child nodes only
+		vbox.setPadding(new Insets(10, 0, 0, 10)); //padding between vbox border and child node
+		
+		TableColumn rankCol = new TableColumn("Rank");
+		TableColumn scoreCol = new TableColumn("Score");
+		TableColumn playerCol = new TableColumn("Player");
+		
+		table.getColumns().addAll(rankCol, scoreCol, playerCol);
+		
+		vbox.getChildren().addAll(table);
+		
+		 ((Group) myScene.getRoot()).getChildren().addAll(vbox);
+		 
+	        primaryStage.setScene(myScene);
+	        primaryStage.show();
+
+		
 		
 	}
 
