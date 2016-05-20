7dba8f9760afec2b77b9eb1f6847dd77308a2daa
Gniweist <tsiewings@gmail.com>
2016-05-20 12:41:19 +0200

    update

--- src/Application/view/TestStartMenyn.java
+++ src/Application/view/TestStartMenyn.java
@@ -1,10 +1,30 @@
 package Application.view;
 
-public class TestStartMenyn {
+import javafx.application.Application; 
+import javafx.beans.value.ObservableValue; 
+import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.scene.control.Slider; 
+import javafx.scene.paint.Color; 
+import javafx.scene.shape.Line; 
+import javafx.scene.shape.StrokeLineCap; 
+import javafx.scene.text.Text; 
+import javafx.stage.Stage; 
+
+public class TestStartMenyn extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Welcome to your Pong adventure");
+		
+		Group root = new Group();
+		Scene myScene = new Scene(root, 600, 800);
+		
 	}
 
 }
