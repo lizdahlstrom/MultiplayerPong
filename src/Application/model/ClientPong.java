9ff42bfb0a9bea57c427a86530a3508ec8432bb8
Gniweist <tsiewings@gmail.com>
2016-05-26 09:32:30 +0200

    The Client class ClientPong created.

--- src/Application/model/ClientPong.java
+++ src/Application/model/ClientPong.java
@@ -1,7 +1,9 @@
 package Application.model;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -11,29 +13,61 @@ import java.rmi.UnknownHostException;
 import Application.controller.TestMainController;
 import Application.view.TestStartMenyn;
 
-public class ClientPong extends Thread {
+public class ClientPong  {
 	
-	private InetAddress ipAddress;
-	private DatagramSocket socket;
-	private TestStartMenyn testStartMenyn;
-	private TestMainController testMainController;
+	InetAddress iadr;
+	DatagramSocket socket;
+	int port;
 	
 	
-	public ClientPong(TestStartMenyn testStartMenyn, String ipAddress) throws UnknownHostException {
-		this.testStartMenyn = testStartMenyn;
+	public ClientPong (String toAddress, int portNr) 
+						throws UnknownHostException, SocketException {
+	//Create a socket for sending and receiving.
 		try {
-			this.socket = new DatagramSocket();
-			this.ipAddress = InetAddress.getByName(ipAddress);
-			}catch (SocketException e) {
-				e.printStackTrace();
-			}catch (java.net.UnknownHostException e) {
-				e.printStackTrace();
+			iadr = InetAddress.getByName(toAddress);
+		} catch (java.net.UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		port = portNr;
+		socket = new DatagramSocket();
+		//pack();
+						}
+	
+	private void report (String name) {
+		String message = String.format("%-10s%", name);
+		byte[] data = message.getBytes(); //Convert the text to bytes.
+		for (int i=1; i<=3;i++) {  //Make three sending attempts.
+			//Create a datagram with the message.
+			DatagramPacket packet = new DatagramPacket(data, data.length, iadr, port);
+			
+			try {
+				socket.send(packet);
+				socket.receive(packet); //Wait until response arrives.
+				//Convert the received message from bytes to String.
+				String response = new String(packet.getData(),0,
+									packet.getLength());
+				if (response.equals("OK"))
+						return;
 			}
+			catch(IOException ie) {} //Timeout or error.			
+		}
+		//Failure of sending data .
+		System.out.println("Issues with the communication");
 	}
-	
-	public void run() {
-	
-	}
+
+
+public static void main(String[]arg)
+				throws UnknownHostException, SocketException {
+	//Get the internetaddresss and port number from arg.
+	new ClientPong (arg[0],Integer.parseInt(arg[1]));
 }
+}
+
+	
+	
+	
+	
+
 
 
