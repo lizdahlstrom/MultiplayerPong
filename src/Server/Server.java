edb7c3a5b2c843771554cecbf0ee92b06a5c42e0
Liz Dahlström <liz.dahlstrom@gmail.com>
2016-05-27 17:12:30 +0200

    Packets.. still working on server logic

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,19 +1,34 @@
-package Server;
+package server;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import server.GameStates.GameState;
+import server.packet.BallPacket;
+import server.packet.PlayerPacket;
+
 public class Server extends Connection {
 
 	// Instance variables
-	static DatagramSocket socket;
+	static GameStates gameState;
+	static DatagramSocket socket; 
 	static DatagramPacket packet;
 	static InetAddress ip;
 	static byte[] buf = new byte[1024];
+	static InetAddress [] clientIps = new InetAddress[2]; // only room for two
 
-	static PlayerModel[] users = new PlayerModel[2]; // room for 2 users
+	static PlayerPacket[] users = new PlayerPacket[2]; // room for 2 users
+
+	//Constructor
+	public Server(){
+		gameState = new GameStates(GameState.PENDING);
+	}
 
 	// MAIN METHOD
 	public static void main(String[] args) throws Exception {
@@ -25,8 +40,55 @@ public class Server extends Connection {
 			try {
 				// Waiting for connection..
 				System.out.println("waiting for incoming data...");
-				socket.receive(packet);
+				socket.receive(packet); // retrieving a packet
 				// Get the data from packet
+				if(getPacketData(packet).getClass() == PlayerPacket.class){ // if it's a playerpacket
+					for(Object user: users){
+						user = getPacketData(packet);
+					}
+					for (InetAddress ip : clientIps) {
+						ip = packet.getAddress();
+					}
+					sendToClients(packet);
+				}
+				else if (getPacketData(packet).getClass() == BallPacket.class) { // if it's a ballpacket
+					BallPacket ballPacket;
+					ballPacket = (BallPacket)getPacketData(packet);
+					sendToClients(ballPacket);
+				}
+				else { // if its something else
+					System.out.println("uknown packet format...");
+				}
+
+				switch (gameState.current) {
+				case PENDING:
+					for(Object user: users){
+						user = getPacketData(packet);
+					}
+
+					if(users != null){
+						gameState.setGameState(GameState.GAMING);
+						//TODO: Send packet with gamestate that can be interpreted from the client side 
+						//to start the game
+
+					}
+					break;
+				case GAMING:
+					// Recieve and send package of player position and ball position to other client
+
+
+					break;
+				case ENDED:
+
+					break;
+				default:
+					break;
+				};
+
+
+
+
+
 				InetAddress ipAddress = packet.getAddress();
 				int clientPort = packet.getPort();
 				String strData = new String(packet.getData());
@@ -48,5 +110,44 @@ public class Server extends Connection {
 
 		}
 
+
+
 	}
+
+	// METHODS
+	public static Object getPacketData(DatagramPacket packet) throws ClassNotFoundException, IOException{
+		ByteArrayInputStream byteArrayInputStream = 
+				new ByteArrayInputStream(packet.getData(), packet.getOffset(), packet.getLength());
+		ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+		//Returning java object
+		return objectInputStream.readObject();
+	}
+
+	public static void sendToClients(Object packet) throws IOException{
+		ByteArrayOutputStream byteArrayOutputStream =
+				new ByteArrayOutputStream();
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+		byte buf[];
+
+
+	}
+
+	public static boolean checkReadyStatus(){
+		Boolean status = false;
+		if (clientIps[0].isAnyLocalAddress() && clientIps[1].isAnyLocalAddress()) {
+			status = true;
+		}
+
+		return status;
+	}
+
+	//	public Object getData(){
+	//	
+	//		byte [] buf = new byte[1024];
+	//		ByteArrayInputStream byteArrayInputStream = 
+	//				new ByteArrayInputStream(packet.getData(), packet.getOffset(), packet.getLength());
+	//		ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+	//		user = objectInputStream.readObject();
+	//		
+	//	}
 }
---
+++ src/server/packet/PlayerPacket.java
@@ -0,0 +1,24 @@
+package server.packet;
+
+import java.io.Serializable;
+
+public class PlayerPacket implements Serializable {
+
+	//Instance variables
+	int x;
+	int y;
+	int velX;
+	int velY;
+	int score;
+	String ip;
+
+	public PlayerPacket(int x, int y, int velX, int velY, int score, String ip){
+		this.x = x;
+		this.y = y;
+		this.velX = velX;
+		this.velY = velY;
+		this.score = score;
+		this.ip = ip;
+	}
+
+}
